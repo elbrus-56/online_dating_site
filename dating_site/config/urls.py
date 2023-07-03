a8652dbda76bb63cd584faa786a26fe1f3e23686
elbrus56 <elbrus56@mail.ru>
2023-07-03 19:58:37 +0500

    Начало проекта 'Сайт знакомств'

---
+++ dating_site/config/urls.py
@@ -0,0 +1,18 @@
+from django.conf.urls.static import static
+from django.contrib import admin
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.urls import path, include
+
+from config import settings
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('api/clients/', include('register_user.urls')),
+]
+
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
+
+    urlpatterns += staticfiles_urlpatterns()
