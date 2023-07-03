f99fd910fbdbeba85e607f8c75847da629cc5fa1
elbrus56 <elbrus56@mail.ru>
2023-07-03 20:08:42 +0500

    Создана пользовательская модель User
    Модель создана на основе стандартной Django модели User.
    
    Добавлены поля:
    	- sex
    	- photo
    
    В качестве login по-умолчанию использовано поле email вместо username.
    
    Поле username стало необязательным.

---
+++ dating_site/register_user/models.py
@@ -0,0 +1,24 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+
+
+class User(AbstractUser):
+    SEX = [
+        ('мужской', 'Мужской'),
+        ('женский', 'Женский')
+
+    ]
+    email = models.EmailField(_('email address'), unique=True)
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username']
+    sex = models.TextField(choices=SEX, verbose_name='Пол', blank=True)
+    photo = models.ImageField(upload_to="images/%Y-%m-%d/", default="images/default.png", max_length=200)
+    username = models.CharField(max_length=150, blank=True)
+
+    class Meta:
+        db_table = "dating_site_user"
+        verbose_name = 'Посетитель'
+        verbose_name_plural = 'Посетители'
+        ordering = ['id']
+
